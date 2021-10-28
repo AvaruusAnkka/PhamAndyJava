61e66225eae26963db42c97effc3fcaaa4d41116
Andy <pmduck@hotmail.com>
2021-10-28 13:32:19 +0300

    Spring with only 1 added package (spring web)

---
+++ kurssi/src/main/java/com/example/kurssi/KurssiApplication.java
@@ -0,0 +1,13 @@
+package com.example.kurssi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurssiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurssiApplication.class, args);
+	}
+
+}
---
+++ kurssi/src/test/java/com/example/kurssi/KurssiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kurssi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurssiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
